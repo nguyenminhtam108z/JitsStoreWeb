943ea31e79608e1394abf5d4c8f972ac108647a2
nguyenminhtam108z <nguyenminhtam108z@gmail.com>
2023-11-25 09:10:57 +0700

    update source

--- JitsController/Program.cs
+++ JitsController/Program.cs
@@ -10,8 +10,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<JitsStoreContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ISupplierRepository, SupplierRepository>();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
